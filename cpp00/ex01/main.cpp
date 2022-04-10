5a03d1dcdb868fa837cf53e3c4c91e9e7a5ad757
Thomas Nanchen <thomasnanchen@hotmail.com>
2022-04-10 20:49:27 +0200

    ex00 ok

---
+++ cpp00/ex01/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int	main(void)
+{
+	std::string cmd;
+
+	while (1)
+	{
+		std::cout << "Type your command: ";
+		std::getline(std::cin, cmd);
+		if (cmd == "EXIT")
+			exit(0);
+		// else if (cmd == "ADD")
+			// ...
+		// else if (cmd == "SEARCH")
+			// ...
+	}
+	return 0;
+}
--- cpp_tutos/iostream.cpp
+++ cpp_tutos/iostream.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 
-// 'std::' comprend toute la bibliothèque standard du c++
-
-char buf[512];
-
 int	main(void)
 {
+	char buf[512];
+
 	std::cout << "Hello world !" << std::endl;
 	std::cout << "Input a word: " << std::endl;
 	std::cin >> buf;
