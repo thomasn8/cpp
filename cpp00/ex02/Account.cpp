017522f4edec2adc7e3cded2f984f9e2372e045f
Thomas Nanchen <thomasnanchen@hotmail.com>
2022-04-17 19:38:32 +0200

    ex02 en cours

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,6 +1,82 @@
 #include "Account.hpp"
+#include <iostream>
+#include <time.h> 
 
-int	main(void)
+
+// **************** Getter functions **************** //
+int		Account::getNbAccounts(void)
 {
-	Account test;
+	return (Account::_nbAccounts);
+}
+
+int		Account::getTotalAmount(void)
+{
+	return (Account::_totalAmount);
+}
+
+int		Account::getNbDeposits(void)
+{
+	return (Account::_totalNbDeposits);
+}
+
+int		Account::getNbWithdrawals(void)
+{
+	return (Account::_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos(void)
+{
+	Account::_displayTimestamp();
+	std::cout << "account:" << Account::getNbAccounts() << ";";
+	std::cout << "total:" << Account::getTotalAmount() << ";";
+	std::cout << "deposits:" << Account::getNbDeposits() << ";";
+	std::cout << "withdrawals:" << Account::getNbWithdrawals() << std::endl;
+}
+
+// **************** Datetime function **************** //
+void	Account::_displayTimestamp(void)
+{
+	time_t		rawtime;
+	struct tm	*timeinfo;
+	char		datetime[15+1];
+
+	time (&rawtime);
+	timeinfo = localtime (&rawtime);
+	strftime(datetime,15+1,"[%Y%m%d_%H%M%S]",timeinfo);
+	std::cout << datetime << " ";
+}
+
+// ***************** Constr./Destr ***************** //
+Account::Account(int initial_deposit)
+{
+	static int	index = 0;
+
+	// attributs de class
+	Account::_nbAccounts += 1;
+	Account::_totalAmount += initial_deposit;
+	Account::_totalNbDeposits += 1;
+	Account::_totalNbWithdrawals += 0;
+
+	// attributs d'instance
+	Account::_accountIndex = ++index;
+	Account::_amount = initial_deposit;
+	Account::_nbDeposits = 1;
+	Account::_nbWithdrawals = 0;
+
+	// logs
+	Account::_displayTimestamp();
+	std::cout << "index:" << Account::_accountIndex;
+	std::cout << ";amount:" << Account::_amount;
+	std::cout << "created" << std::endl;
+}
+
+// Account::Account(void)
+// {
+// 	return;
+// }
+
+Account::~Account(void)
+{
+	Account::_nbAccounts -= 1;
+	return;
 }
