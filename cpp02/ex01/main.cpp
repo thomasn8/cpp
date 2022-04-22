0bb3c03a48dc368c588bd3ee27f4ca2f23216fab
Thomas Nanchen <thomasnanchen@hotmail.com>
2022-04-22 14:44:58 +0200

    mis a jour ex00 du cpp02 conforme à ex01

--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -2,16 +2,18 @@
 
 int main( void ) 
 {
-	Fixed a;
-	Fixed const b( 10 );
-	Fixed const c( 42.42f );
-	Fixed const d( b );
-	a = Fixed( 1234.4321f );
+	Fixed a;					// Default
+	Fixed const b( 10 );		// Int
+	Fixed const c( 42.42f );	// Float
+	Fixed const d( b );			// Copy + Assignement
+	a = Fixed( 1234.4321f );	// Float + Assignement + Destructor
 
 	std::cout << "a is " << a << std::endl;
 	std::cout << "b is " << b << std::endl;
 	std::cout << "c is " << c << std::endl;
 	std::cout << "d is " << d << std::endl;
+
+	std::cout << "b_prt = " << &b << " | d_ptr = " << &d << std::endl;
 
 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
