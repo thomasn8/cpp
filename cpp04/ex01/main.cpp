932bbf2ef6f034cfe9e2b12ea8c371417250f7c5
Thomas Nanchen <thomasnanchen@hotmail.com>
2022-05-03 21:10:04 +0200

    cpp04, ex01 ok

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,6 +6,17 @@
 
 int main()
 {
+	// SUBJECT'S MAIN
+	std::cout << std::endl;
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	delete j;
+	delete i;
+	std::cout << std::endl << std::endl;
+
+
+	// MY MAIN
 	int	N = 10;
 	Animal	*animals[N];
 
