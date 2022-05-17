aa6655dd954a8b06d0a20f33e128d8db3c3a640a
tnanchen <thomasnanchen@hotmail.com>
2022-05-17 15:20:32 +0200

    cpp06 ok

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -1,6 +1,7 @@
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
+#include <stdexcept>
 
 // Crée aléatoirement une instance de A, B ou C et la retourne en tant que pointeur sur Base
 Base *generate(void)
@@ -22,35 +23,68 @@ Base *generate(void)
 }
 
 // affiche le véritable type de l’objet pointé par p : "A", "B" ou "C"
-// (utiliser un pointeur dans cette fonction est interdit)
 void identify(Base* p)
 {
-	// if (dynamic_cast<A *>(p))
-	// 	std::cout << "A class" << std::endl;
-	// if (dynamic_cast<B *>(p))
-	// 	std::cout << "B class" << std::endl;
-	// if (dynamic_cast<C *>(p))
-	// 	std::cout << "C class" << std::endl;
+	try
+	{
+		A & a = dynamic_cast<A &>(*p);
+		std::cout << "A class" << std::endl;
+	}
+	catch (std::bad_cast &bc)
+	{
+	}
+	try
+	{
+		B & b = dynamic_cast<B &>(*p);
+		std::cout << "B class" << std::endl;
+	}
+	catch (std::bad_cast &bc)
+	{
+	}
+	try
+	{
+		C & c = dynamic_cast<C &>(*p);
+		std::cout << "C class" << std::endl;
+	}
+	catch (std::bad_cast &bc)
+	{
+	}
+}
 
-	
+// affiche le véritable type de l’objet pointé par p : "A", "B" ou "C"
+void identify(Base& p)
+{
+	try
+	{
+		A & a = dynamic_cast<A &>(p);
+		std::cout << "A class" << std::endl;
+	}
+	catch (std::bad_cast &bc)
+	{
+	}
+	try
+	{
+		B & b = dynamic_cast<B &>(p);
+		std::cout << "B class" << std::endl;
+	}
+	catch (std::bad_cast &bc)
+	{
+	}
+	try
+	{
+		C & c = dynamic_cast<C &>(p);
+		std::cout << "C class" << std::endl;
+	}
+	catch (std::bad_cast &bc)
+	{
+	}
 }
 
 int main()
 {
-	Base *p = generate();
-	identify(p);
-
+	Base *ptr = generate();
+	Base & ref = *ptr;
+	identify(ptr);
+	identify(ref);
 	return 0;
 }
-
-/* 	
-	void identify(Base* p);
-	Elle affiche le véritable type de l’objet pointé par p : "A", "B" ou "C".
-	
-	void identify(Base& p);
-	Elle affiche le véritable type de l’objet pointé par p : "A", "B" ou "C". Utiliser un pointeur
-	dans cette fonction est interdit.
-	
-	Le fichier d’en-tête typeinfo est interdit.
-	Écrivez un programme pour tester que tout fonctionne comme attendu.
-*/
