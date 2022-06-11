76225ae7281f881da5f9a028f1dcf01b25d8b0a0
Thomas Nanchen <thomasnanchen@hotmail.com>
2022-06-11 15:55:43 +0200

    cpp7 terminé

--- cpp7/ex02/Array.hpp
+++ cpp7/ex02/Array.hpp
@@ -2,7 +2,99 @@
 # define ARRAY_HPP
 
 #include <iostream>
+#include <stdexcept>
 
-template
+# define RED "\033[0;31m"
+# define WHI "\033[0m"
+
+template<typename T = int>
+class Array
+{
+	public:
+
+		unsigned int size() const { return this->_n; }
+
+		T getFirst() const
+		{
+			if (this->_n > 0)
+				return this->_array[0];
+			return 0;
+		}
+
+		class InaccessibleIndex : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("index out of array");
+				}
+		};
+
+		T & operator[](unsigned int index)
+		{
+			try
+			{
+				if (index >= this->_n)
+					throw Array::InaccessibleIndex();
+			}
+			catch (const Array::InaccessibleIndex & e)
+			{
+				std::cerr << RED;
+				std::cerr << "Error: ";
+				std::cerr << e.what();
+				std::cerr << ". Program exited";
+				std::cerr << std::endl;
+				std::cerr << WHI;
+				exit(1);
+			}
+			return this->_array[index];
+		}
+
+		Array & operator=(Array const & src)
+		{
+			this->_n = src.size();
+			this->_array = new T[src.size()];
+			std::cout << "(" << this << " - assign.) Array (" << &src << ") copied" << std::endl;
+			return *this;
+		}
+
+		Array(Array const & src) :
+		_n(src.size())
+		{
+			this->_array = new T[src.size()];
+			std::cout << "(" << this << " - copy) Array created" << std::endl;
+		}
+
+		Array(unsigned int  n) : _n(n)
+		{
+			this->_array = new T[n];
+			for (unsigned int i = 0; i < n; i++)
+				this->_array[i] = 0;
+			std::cout << "(" << this << " - N) Array created" << std::endl;
+		}
+
+		Array() : _n(0)
+		{
+			this->_array = NULL;
+			std::cout << "(" << this << " - default) Null Array created" << std::endl;
+		}
+
+		~Array() 
+		{
+			delete [] this->_array;
+			std::cout << "(" << this << " - default) Array destroyed" << std::endl;
+		}
+	
+	private:
+		T *_array;
+		unsigned int _n;
+};
+
+template<typename T>
+std::ostream	& operator<<(std::ostream & o, Array<T> const & instance)
+{
+	o << "Array of " << instance.size() << " elements";
+	return o;
+}
 
 #endif
--- cpp7/ex02/main.cpp
+++ cpp7/ex02/main.cpp
@@ -2,6 +2,46 @@
 
 int main()
 {
+	Array<> empty_array;
+	std::cout << empty_array << std::endl;
+	// empty_array[0] = 1;											// Tentative d'accès à de la mémoire non-attribuée
+	std::cout << std::endl;
 
+	Array<int> int_array = Array<int>(3);
+	std::cout << int_array << std::endl;
+	std::cout << "int_array[0] = " << int_array[0] << std::endl;
+	int_array[0] = 1;
+	std::cout << "int_array[0] = " << int_array[0] << std::endl;
+	std::cout << "int_array[1] = " << int_array[1] << std::endl;
+	int_array[1] = 2;
+	std::cout << "int_array[1] = " << int_array[1] << std::endl;
+	std::cout << "int_array[2] = " << int_array[2] << std::endl;
+	int_array[2] = 3;
+	std::cout << "int_array[2] = " << int_array[2] << std::endl;
+	std::cout << std::endl;
+	
+	Array<float> float_array = Array<float>(2);
+	std::cout << float_array << std::endl;
+	std::cout << "float_array[0] = " << float_array[0] << std::endl;
+	float_array[0] = 1.111f;
+	std::cout << "float_array[0] = " << float_array[0] << std::endl;
+	std::cout << "float_array[1] = " << float_array[1] << std::endl;
+	float_array[1] = 2.222;
+	std::cout << "float_array[1] = " << float_array[1] << std::endl;
+	std::cout << std::endl;
+
+	Array<char> char_array = Array<char>(5);
+	std::cout << char_array << std::endl;
+	std::cout << "char_array[0] = " << char_array[0] << std::endl;
+	char_array[0] = 'a';
+	std::cout << "char_array[0] = " << char_array[0] << std::endl;
+	std::cout << "char_array[3] = " << char_array[2] << std::endl;
+	char_array[2] = 'c';
+	std::cout << "char_array[3] = " << char_array[2] << std::endl;
+	std::cout << "char_array[5] = " << char_array[5] << std::endl;	// Tentative d'accès à de la mémoire non-attribuée
+	char_array[5] = 'f';
+	std::cout << "char_array[5] = " << char_array[5] << std::endl;
+	std::cout << std::endl;
+	
 	return 0;
 }
