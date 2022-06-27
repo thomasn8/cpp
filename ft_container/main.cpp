473375d7f6a73fc87eeece9ab9e3358b125d7c57
Thomas Nanchen <thomasnanchen@hotmail.com>
2022-06-27 23:12:32 +0200

    containers, vector work in progress

--- ft_container/main.cpp
+++ ft_container/main.cpp
@@ -1,14 +1,27 @@
-
 #include "vector.hpp"
-#include "map.hpp"
 #include "Test.hpp"
 
 using namespace std;
 
 int main()
 {
-	// ft::vector<int> vec1;
-	// cout << endl;
+	ft::vector<int> vec1(5, 10);
+	cout << &vec1[0] << ": " << vec1[0] << endl;
+	cout << &vec1[1] << ": " << vec1[1] << endl;
+	cout << &vec1[2] << ": " << vec1[2] << endl;
+	cout << &vec1[3] << ": " << vec1[3] << endl;
+	cout << &vec1[4] << ": " << vec1[4] << endl;
+	cout << endl;
+
+	ft::vector<int>::iterator it1(&vec1[0]);
+	ft::vector<int>::iterator it2(vec1.end());
+	cout << *it1 << endl;
+	cout << *it2 << endl;
+	// cout << *it << endl;
+	// ++it;
+	// cout << *it << endl;
+	// cout << it[0] << " " << it[1] << endl;
+
 	
 	// ft::vector<int> vec2(5, 10);
 	// cout << vec2 << ": " << *vec2 << endl << endl;
@@ -20,23 +33,20 @@ int main()
 	// // cout << &vec2[5] << ": " << vec2[5] << endl;
 	// cout << endl;
 
-	cout << sizeof(Test) << endl;
-	ft::vector<Test> vec3(3, 10);
-	cout << endl;
-	
-	// ft::map<int> map1;
+	// cout << sizeof(Test) << endl;
+	// ft::vector<Test> vec3(3, 10);
 	// cout << endl;
 
-	ft::vector<Test> myvector;
-	Test * p;
-	unsigned int i;
-	// allocate an array with space for 5 elements using vector's allocator:
-	p = myvector.get_allocator().allocate(5);
-	// construct values in-place on the array:
-	for (i=0; i<5; i++) myvector.get_allocator().construct(&p[i],i);
-	cout << "The allocated array contains:";
-	for (i=0; i<5; i++) cout << ' ' << p[i];
-	cout << endl;
+	// ft::vector<Test> myvector;
+	// Test * p;
+	// unsigned int i;
+	// // allocate an array with space for 5 elements using vector's allocator:
+	// p = myvector.get_allocator().allocate(5);
+	// // construct values in-place on the array:
+	// for (i=0; i<5; i++) myvector.get_allocator().construct(&p[i],i);
+	// cout << "The allocated array contains:";
+	// for (i=0; i<5; i++) cout << ' ' << p[i];
+	// cout << endl;
 
 	return 0;
 }
@@ -86,6 +96,11 @@ int main()
 // 	cout << &vec2[4] << ": " << vec2[4] << endl;
 // 	cout << &vec2[5] << ": " << vec2[5] << endl;
 // 	cout << endl;
+
+// 	std::vector<int>::iterator it(vec2.begin());
+// 	cout << *it << endl;
+// 	cout << it[0] << " " << it[1] << endl;
+
 
 // 	return 0;
 // }
--- ft_container/vector.hpp
+++ ft_container/vector.hpp
@@ -4,23 +4,13 @@
 
 #include <memory>
 #include <iostream>
+#include <iterator>
 
 # define RED "\033[0;31m"
 # define WHI "\033[0m"
 
 using namespace std;
 
-/* 
-
-default (1)	
-explicit vector (const allocator_type& alloc = allocator_type());				-> Constructs an empty container, with no elements.
-			alloc
-				Allocator object.
-				The container keeps and uses an internal copy of this allocator.
-				Member type allocator_type is the internal allocator type used by the container, defined in vector as an alias of its second template parameter (Alloc).
-				If allocator_type is an instantiation of the default allocator (which has no state), this is not relevant.
-
-*/
 namespace ft
 {
 	template <typename T, typename Alloc = allocator<T> >
@@ -33,10 +23,10 @@ namespace ft
 			typedef unsigned int size_type;											// can represent any non-negative value of difference_type
 
 			typedef allocator<T> 								allocator_type;		// l'allocateur par défault
-			typedef typename allocator_type::reference 			reference;			// T& -> Reference to element
+			typedef typename allocator_type::reference 			reference;			// T & -> Reference to element
 			typedef typename allocator_type::const_reference 	const_reference;	// const T& -> Reference to constant element
-			typedef	typename allocator_type::pointer 			pointer;			// T* Pointer to element
-			typedef	typename allocator_type::const_pointer 		const_pointer;		// cont T* Pointer to constant element
+			typedef	typename allocator_type::pointer 			pointer;			// T * Pointer to element
+			typedef	typename allocator_type::const_pointer 		const_pointer;		// const T * Pointer to constant element
 			
 			typedef int											difference_type;	// Difference between two pointers (a signed integral type), identical to: iterator_traits<iterator>::difference_type /
 
@@ -45,16 +35,77 @@ namespace ft
 			// typedef reverse_iterator<iterator> 			reverse_iterator;
 			// typedef reverse_iterator<const_iterator> 			const_reverse_iterator;
 
+		// ITERATORS
+			class iterator : public std::iterator<std::random_access_iterator_tag, T>
+			{
+				private:
+					T * _p;
+				
+				public:
+				// CONSTRUCTEURS/DESTRUCTEUR
+					iterator() : _p(0) {cout << "Iterator default" << endl;}																// default
+					iterator(T * x) : _p(x) {cout << "Iterator pointer" << endl;}															// special
+					iterator(const iterator & it) : _p(it._p) {cout << "Iterator copy" << endl;}											// copy
+					iterator & operator=(iterator const & src) { this->_p = src.getP(); return *this; cout << "Iterator assign" << endl;} 	// assign 
+					~iterator() { cout << "Iterator destruction" << endl;}
+				// ACCESSORS
+					T * getP() const { return this->_p; }
+
+				// SURCHARGES
+					iterator& operator++() { ++this->_p; return *this; }
+					iterator operator++(T) { iterator tmp(*this); this->_p++; return tmp; }
+					iterator & operator--() { --this->_p; return *this; }
+					iterator operator--(T) { iterator tmp(*this); this->_p--; return tmp; }
+					bool operator==(const iterator & rhs) const { return this->_p == rhs._p; }
+					bool operator!=(const iterator & rhs) const { return this->_p != rhs._p; }
+					bool operator<(const iterator & rhs) const { return this->_p < rhs._p; }
+					bool operator<=(const iterator & rhs) const { return this->_p <= rhs._p; }
+					bool operator>(const iterator & rhs) const { return this->_p > rhs._p; }
+					bool operator>=(const iterator & rhs) const { return this->_p >= rhs._p; }
+					T & operator[](unsigned int index) { return this->_p[index]; }	
+					int & operator*() { return *_p; }
+					// *a++ Two iterators that compare equal, keep comparing equal after being both increased.
+					// *a-- Can be decremented (if a dereferenceable iterator value precedes it).
+					// ARITHMETIC
+					// a + n
+					// n + a
+					// a - n
+					// a - b
+					// a += n
+					// a -= n
+			};
+
+		iterator begin() { iterator it(this->_first); return it; };
+		iterator end() { iterator ite(this->_last); return ite; };
+
+
 		// CONSTRUCTEURS/DESTRUCTEUR
+
+			/*	(3) range constructor :
+    			Constructs a container with as many elements as the range [first,last), with each element constructed from its corresponding element in that range, in the same order. 
+				
+				first, last :
+				Input iterators to the initial and final positions in a range. The range used is [first,last), which includes all the elements between first and last, including the element pointed by first but not the element pointed by last.
+				The function template argument InputIterator shall be an input iterator type that points to elements of a type from which value_type objects can be constructed.
+
+				Prototype :
+				vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()); 
+			*/
+			// template <typename InputIterator>
+			// vector<T>(InputIterator first, InputIterator last)
+			// {
+			// 	(void) first;
+			// 	(void) last;
+			// 	cout << "(" << this << " - range) vector created" << endl;
+			// }
+
 			vector<T>(size_type n, const value_type & val) : _n(n)
 			{
-				// size_type s = this->_difference_type();
 				this->_pointer = this->_alloc.allocate(n);					// créer un objet T pour le copier n fois, puis détruit l'objet de base
 				this->_first = this->_pointer;
 				for (size_type i = 0; i < n; i++)
 				{
-					this->_alloc.construct(this->_pointer, val);
-					// this->_alloc.construct(this->_pointer, val);			// utilise l'objet créé pour Construire les instances par copie
+					this->_alloc.construct(this->_pointer, val+i);			// utilise l'objet créé pour Construire les instances par copie
 					this->_pointer++;
 				}
 				this->_pointer--;
@@ -65,7 +116,7 @@ namespace ft
 			vector<T>() : _n(0)
 			{
 				this->_first = 0;
-				cout << "(" << this << " - default) Null vector created" << endl;
+				cout << "(" << this << " - null) vector created" << endl;
 			}
 
 			virtual ~vector<T>()
@@ -73,37 +124,39 @@ namespace ft
 				cout << "(" << this << " - default) vector destroyed" << endl;
 			}
 
+		// ELEMENT ACCESS:
+			reference front() { return *this->_first; }
+			const_reference front() const { return *this->_first; }
+			reference back() { return *this->_last; }
+			const_reference back() const { return *this->_last; }
+
 		// SURCHARGES
-			T & operator*() const			{ return *this->_first; }
+			reference operator*() const			{ return *this->_first; }
 			T * operator&() const			{ return &this->_first; }
-			T & operator[](size_type index)	{ return this->_first[index]; }
+			reference operator[](size_type index)	{ return this->_first[index]; }
 
 		// ALLOCATOR
-			allocator_type get_allocator() const { return this->_alloc; }
-			// for (i=0; i<5; i++)
-			// 	myvector.get_allocator().construct(&p[i],i)		->		permet de construire en utilisant un autre Constructeur que celui par copie
+			allocator_type get_allocator() const { return this->_alloc; }	// for (i=0; i<5; i++) myvector.get_allocator().construct(&p[i],i)	
+																			// permet de construire en utilisant un autre Constructeur que celui par copie
 
-
-			T *				_first;
-			T *				_last;
 		private :
 
 			allocator_type	_alloc;
 			size_type		_n;
-			// T *				_first;	
-			// T *				_last;	
+			T *				_first;	
+			T *				_last;
 			T *				_pointer;
 			
 			size_type _difference_type() const
 			{
 				return sizeof(T);
-			};
+			}
 	};
 
 	template <typename T>
 	ostream	& operator<<(ostream & o, ft::vector<T> const & inst)
 	{
-		cout << inst._first;
+		cout << &inst.front();
 		return o;
 	}
 }
